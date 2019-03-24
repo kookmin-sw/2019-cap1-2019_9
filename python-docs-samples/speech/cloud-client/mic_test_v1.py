b75e6d46a8c8aac24576d0c22897263461ce3b9e
s3-20132872 <39684876+s3-20132872@users.noreply.github.com>
2019-03-24 16:01:35 +0900

    Add files via upload

--- python-docs-samples/speech/cloud-client/mic_test_v1.py
+++ python-docs-samples/speech/cloud-client/mic_test_v1.py
@@ -1,19 +1,53 @@
+# -*- coding: utf-8 -*-
 import micspeech_v1
 import time
+from google.cloud import translate
+#번역 모듈 가져옴
+#from googletrans import Translator
+
+
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 def main():
     flag = 1
     while flag:
         speech = micspeech_v1.speech_v1()
+
+        translate_client = translate.Client() #번역모듈 변수 선언
+    #    translator = Translator()
+
         while True:
             text = speech.getText()
+
+            #text = u'Hello, world!'
+            target =  ['en', 'ja', 'es'] #번역할 언어 배열로 선언
+
             if text is None:
                 break
             print(text)
+
+            # Translates some text into Russian
+
+            print("\n google translate api test")
+            for i in target:
+                #언어에 맞게 번역 실시
+                translation = translate_client.translate(
+                    text,
+                    target_language=i)
+
+                print(u'Translation: {}'.format(translation['translatedText'].decode("UTF-8").encode("UTF-8"))) #번역된 결과 출력
+            # [END translate_quickstart]
+
+            #print(translator.translate(utext, src='ko', dest='ja'))
+
+
             time.sleep(0.01)
-            # if ('end' or '끝' in text):
-            #     flag = 0
-            #     break
+            if ('end' in text):
+                 flag = 0
+                 break
+
 
 
 if __name__ == '__main__':
