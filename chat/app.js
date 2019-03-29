ac58339d50193e39cc0c4c04a88624abaafee29b
rhwnsrb <43873104+rhwnsrb@users.noreply.github.com>
2019-03-29 16:06:54 +0900

    Add files via upload

--- chat/app.js
+++ chat/app.js
@@ -1,13 +1,22 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const utf8 = require('utf8');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 let room = ['room1', 'room2'];
 let a = 0;
-
+let data = '';
+const spawn = require("child_process").spawn;
+const pythonProcess = spawn("python", ["/home/kjk/pyaudio/2019-cap1-2019_9-chatting/chat/mic_test_v2.py"]);
+str = pythonProcess.stdout.on('data', (data) => {
+  console.log("11");
+  data = data.toString();
+  console.log((data.toString()));
+  io.to(room[0]).emit('chat message', 1, data);
+});
 
 app.get('/', (req, res) => {
   res.render('chat');
@@ -37,6 +46,7 @@ io.on('connection', (socket) => {
 
   socket.on('chat message', (num, name, msg) => {
     a = num;
+    msg = data;
     io.to(room[a]).emit('chat message', name, msg);
   });
 });
